73e0dfabc1f17290fc51612848ed671c5298cba0
PhuPT <phu@hdint.net>
2017-08-18 16:59:33 +0700

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -46,8 +46,8 @@ gulp.task('sass', function () {
 		.pipe(wait(1000))
 		.pipe(sass({
 			outputStyle: 'expanded',
-			indentType: 'tab',
-			indentWidth: 1
+			indentType: 'space',
+			indentWidth: 4
 		}))
 		.pipe(autoprefixer({
 			browsers: ['last 10 versions']
@@ -56,14 +56,7 @@ gulp.task('sass', function () {
 		.pipe(gulp.dest(paths.bundles))
 		.pipe(rename('style.min.css'))
 		.pipe(cssnano({zindex: false}))
-		.pipe(gulp.dest(paths.min))
-		.pipe(browserSync.stream());
-});
-
-gulp.task('babeljs', function () {
-	return gulp.src( paths.js + '/*.js' )
-		.pipe(babel())
-		.pipe(gulp.dest( paths.temp + '/babeljs' ));
+		.pipe(gulp.dest(paths.min));
 });
 
 gulp.task('vendor_js', function () {
@@ -80,7 +73,21 @@ gulp.task('vendor_js', function () {
 		.pipe(gulp.dest(paths.min));
 });
 
-gulp.task('js', function () {
+gulp.task('jshint', function () {
+	return gulp.src(paths.js + '/*.js')
+		.pipe(jshint({
+			esversion: 6
+		}))
+		.pipe(jshint.reporter('default'))
+});
+
+gulp.task('babeljs', ['jshint'], function () {
+	return gulp.src( paths.js + '/*.js' )
+		.pipe(babel())
+		.pipe(gulp.dest( paths.temp + '/babeljs' ));
+});
+
+gulp.task('js', ['babeljs'], function () {
 	return gulp.src([
 			paths.temp + '/babeljs/*.js'
 		])
@@ -100,24 +107,20 @@ gulp.task('htmlinclude', function() {
 		.pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('jshint', function () {
-	return gulp.src(paths.js + '/*.js')
-		.pipe(jshint({
-			esversion: 6
-		}))
-		.pipe(jshint.reporter('default'))
-});
-
-gulp.task('jsreload', ['jshint', 'babeljs', 'js'], function (done) {
-	browserSync.reload();
-	done();
-});
-
 gulp.task('default', ['vendor_js', 'vendor_css', 'htmlinclude', 'jshint', 'babeljs', 'js', 'sass'], function() {
 
+	// Run server
 	browserSync.init({
 		server: "./dist"
 	});
+
+	// Run registerd tasks
+	gulp.watch([
+		paths.views + '/*/*.html',
+		paths.views + '/*.html'
+	], {cwd: './'}, ['htmlinclude']);
+
+	gulp.watch([paths.js + '/*.js'], {cwd: './'}, ['js']);
 
 	gulp.watch([
 		paths.scss + '/*.scss',
@@ -125,13 +128,11 @@ gulp.task('default', ['vendor_js', 'vendor_css', 'htmlinclude', 'jshint', 'babel
 		paths.scss + '/*/*/*.scss'
 	], {cwd: './'}, ['sass']);
 
-	gulp.watch([paths.js + '/*.js'], {cwd: './'}, ['jsreload']);
-
+	// Hot reload
 	gulp.watch([
-		paths.views + '/*/*.html',
-		paths.views + '/*.html'
-	], {cwd: './'}, ['htmlinclude']);
-
-	gulp.watch(paths.dist + '/*.html').on('change', browserSync.reload);
+		paths.dist + '/*.html',
+		paths.bundles + '/*.js',
+		paths.bundles + '/*.css'
+	]).on('change', browserSync.reload);
 
 });
--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,7 @@
 (function($) {
 
 	$(function() {
-
+       
 	});
 
 	$(window).load(function() {
